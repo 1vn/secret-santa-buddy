6d0768fea5dd7793bdbb5cd91006bd6e4c4239c0
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2014-12-08 21:40:04 -0500

    new project practicing mvc

---
+++ public/javascripts/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var people = []
+  $("#addPeopleButton").click(function() {
+    console.log("Button pressed")
+    people.push($("#input").text())
+    console.log(people)
+    $("#input").empty()
+  })
+
+  $("#outputButton").click(function() {
+    //Put people into db
+    people = []
+  })
+})
