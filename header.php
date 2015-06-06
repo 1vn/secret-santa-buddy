e85446defb3ab15f18763d3ded76e86c5017034a
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-06-06 00:27:14 -0400

    rebuild

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!doctype html>
+<html>
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type"></meta>
+	<title>
+		Secret Santa Buddy by Ivan
+	</title>
+	<meta content="width=device-width, initial-scale=1.0, user-scalable=no" name="viewport"></meta>
+	<link type="text/css" href="style.css" rel="stylesheet"></link>
+</head>
+<body>
+	<div id="mmenu" class="mmenu">
+		<div class="mmenu-item">yo</div>
+	</div>
+	<div id="page">
+		<div id="header">
+			<div id="header-bar_">
+				<div class="cwidth">
+					<div id="header-bar">
+							<a href="index.php">Home</a>
+							<div class="mmenu-button" onclick="showmmenu">x</div>
+					</div>
+				</div>
+			</div>
+		</div>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include 'header.php';
+?>
+	<div class="cwidth">
+		<div class="title-block">
+			<span class="title">Secret Santa Buddy!</span>
+		</div>
+	</div>
+<?php
+include 'footer.php';
+?>
