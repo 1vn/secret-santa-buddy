3cd8c347b2ea8f9ad3557a5cd25e590cc1eaac0d
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-10-14 10:41:02 -0400

    pair matching

--- icl/santasend.inc.php
+++ icl/santasend.inc.php
@@ -2,7 +2,19 @@
 function santasend(){
 	$namelist=explode(",", $_GET['namelist']);
 	$emaillist=explode(",", $_GET['emaillist']);
-	print_r($namelist);
-	print_r($emaillist);
+	$nameemail=array();
+	foreach($namelist as $idx=>$name){
+		$nameemail[$name]=$emaillist[$idx];
+	}	
+
+	$matches=array();
+
+	shuffle($namelist);
+	$matches[$namelist[count($namelist)-1]]=array("name"=>$namelist[0], "email"=>$nameemail[$namelist[0]]);
+	for($i=0; $i<count($namelist)-1;$i++){
+		$matches[$namelist[$i]]=array("name"=>$namelist[$i+1], "email"=>$nameemail[$namelist[$i+1]]);
+	}
+	print_r($matches);
+
 }
 ?>
